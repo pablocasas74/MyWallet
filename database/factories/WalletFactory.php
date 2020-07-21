d5b24b1aa0199dc88d17d55ad8857c0bea6b81d0
pablocasas74 <pablocasas74@yahoo.com.ar>
2020-07-21 14:58:08 -0300

    my first commit

---
+++ database/factories/WalletFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+
+use Faker\Generator as Faker;
+use App\Wallet;
+
+$factory->define(Wallet::class, function (Faker $faker) {
+    return [
+        'money' => $faker->numberBetween($min=500, $max=900)
+    ];
+});
